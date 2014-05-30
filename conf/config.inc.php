c780bfad16578d24acc0f19e788345273c3ddf01
liushaochen <liushaochen1987@qq.com>
2014-05-30 13:43:16 +0800

    重建代码

--- conf/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: shaochen.liu
- * Date: 14-5-28
- * Time: 下午3:34
- */
-return array();
--- core/conf/define.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: shaochen.liu
- * Date: 14-5-30
- * Time: 上午11:09
- */
-defined('APP_PATH') or define('APP_PATH',__ROOT__.'/application');
-defined('CACHE_PATH') or define('CACHE_PATH',__ROOT__.'/cache');
