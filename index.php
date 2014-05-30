72b35cc22bde51d1e4680749862c7a8c962eb238
liushaochen <liushaochen1987@qq.com>
2014-05-30 13:17:42 +0800

    add code
    代码雏形，目前业务全是测试

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaochen.liu
+ * Date: 14-5-28
+ * Time: 下午3:19
+ */
+define("__ROOT__",$_SERVER['DOCUMENT_ROOT']);
+include_once(__ROOT__.'/core/index.php');
+$config = include_once(__ROOT__."/conf/config.inc.php");
