5ca99935ccd9bda85b6bd5ae236b33ac0fea550d
liushaochen <liushaochen1987@qq.com>
2014-06-16 20:16:40 +0800

    1. 添加静态模块；

---
+++ application/html/controller/ListController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liushaochen
+ * Date: 14-6-16
+ * Time: 20:15
+ */
+
+class ListController {
+
+    public function index(){
+        $this->display();
+    }
+} 
