8f5381dfc5c447856c18b912c1ef830db474f335
liushaochen <liushaochen1987@qq.com>
2014-06-16 20:10:59 +0800

    1. 添加静态模块；

---
+++ application/html/controller/IndexController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liushaochen
+ * Date: 14-6-16
+ * Time: 20:04
+ */
+
+class IndexController extends Controller {
+
+    public function  index(){
+        $this->index();
+    }
+} 
