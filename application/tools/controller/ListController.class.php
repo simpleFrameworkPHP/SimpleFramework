876012973ff28b6b5828255652dde094faf639c0
liushaochen <liushaochen1987@qq.com>
2014-06-19 16:02:01 +0800

    将示例完善；

--- application/tools/controller/ListController.class.php
+++ application/tools/controller/ListController.class.php
@@ -9,13 +9,7 @@
 class ListController extends Controller {
 
     public function index(){
-        $model = M();
-        $data = $model->table(array('adfs'=>'GLOBAL_STATUS'))->fields('VARIABLE_NAME AS a')->select();
-        print_r($model->db->sql_str.'<br/>');
-        $data = $model->table(array('adfs'=>'GLOBAL_STATUS'))->fields(array('VARIABLE_NAME AS a'))->select();
-        print_r($model->db->sql_str.'<br/>');
-        $data = $model->table(array('adfs'=>'GLOBAL_STATUS'))->fields(array('a'=>'VARIABLE_NAME'))->where(array('VARIABLE_NAME'=>0,'VARIABLE_VALUE'=>0))->order(array('VARIABLE_VALUE'=>'abs'))->group('VARIABLE_VALUE')->select();
-        print_r($model->db->sql_str.'<br/>');
+
         $this->display();
     }
 } 
