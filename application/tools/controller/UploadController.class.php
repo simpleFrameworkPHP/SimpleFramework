64941b2a8b16c35d27301c18738a7371bdcb4d44
liushaochen <liushaochen1987@qq.com>
2014-07-21 17:15:21 +0800

    修改BUG，增加常量渲染；

---
+++ application/tools/controller/UploadController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaochen.liu
+ * Date: 14-7-21
+ * Time: 下午3:42
+ */
+
+class UploadController extends Controller {
+
+    public function upFile(){
+
+    }
+
+} 
