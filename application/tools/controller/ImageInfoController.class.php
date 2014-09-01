b6c5f82a58f8f3301d1496d9d8c485904ae6113e
liushaochen <liushaochen1987@qq.com>
2014-09-01 18:50:04 +0800

    修改url方式，添加端口判断； 添加图片GPS信息获取示例的链接； 将图片改为多张图片共同赋值的形式；

--- application/tools/controller/ImageInfoController.class.php
+++ application/tools/controller/ImageInfoController.class.php
@@ -9,84 +9,18 @@
 class ImageInfoController extends Controller {
 
     public function getGPSInfo(){
-        $imagePath = '/image/2014/08/31/1409474311596119.jpg';
-        $info = exif_read_data(DATA_PATH.$imagePath,0,true);
-//        $info = exif_read_data(DATA_PATH.'/image/2014/08/31/1.jpg',0,true);var_dump($info);
-        $gps = $info['GPS'];
-        var_dump($gps);
-        //纬度计算
-        $latitude = $this->getGps($gps['GPSLatitude']);
-        if($gps['GPSLatitudeRef'] <> "N"){
-            $latitude = -$latitude;
+        $path = DATA_PATH.'/image/2014/08/31/';
+        $url = './data/image/2014/08/31/';
+        $dir = opendir($path);
+        while($file = readdir($dir)){
+            if(!in_array($file,array('.','..'))){
+                $image = new Image($path.$file);
+                $ipoint = $image->getGPSInfo();
+                $ipoint['path'] = $url.$file;
+                $point[] = $ipoint;
+            }
         }
-        //经度计算
-        $longitude = $this->getGps($gps['GPSLongitude']);
-        if($gps['GPSLongitudeRef'] <> "E"){
-            $longitude = -$longitude;
-        }
-        var_dump($latitude);
-        var_dump($longitude);
-        $this->assign('point',array('y'=>$latitude,'x'=>$longitude,'path'=>'./data'.$imagePath));
+        $this->assign('point',$point);
         $this->display();
-    }
-
-    function floatGps($degrees = 0,$minutes = 0,$seconds = 0){
-        $point = 0;
-        if($degrees <> 0){
-            $point += $degrees;
-        }
-        if($minutes <> 0){
-            $point += $minutes / 60;
-        }
-        if($seconds <> 0){
-            $point += $seconds / 60 / 60;
-        }
-        return $point;
-    }
-
-    //Pass in GPS.GPSLatitude or GPS.GPSLongitude or something in that format
-    function getGps($exifCoord,$isflaot = ture)
-    {
-        $degrees = count($exifCoord) > 0 ? $this->gps2Num($exifCoord[0]) : 0;
-        $minutes = count($exifCoord) > 1 ? $this->gps2Num($exifCoord[1]) : 0;
-        $seconds = count($exifCoord) > 2 ? $this->gps2Num($exifCoord[2]) : 0;
-
-        //normalize
-        $minutes += 60 * ($degrees - floor($degrees));
-        $degrees = floor($degrees);
-
-        $seconds += 60 * ($minutes - floor($minutes));
-        $minutes = floor($minutes);
-
-        //extra normalization, probably not necessary unless you get weird data
-        if($seconds >= 60)
-        {
-            $minutes += floor($seconds/60.0);
-            $seconds -= 60*floor($seconds/60.0);
-        }
-
-        if($minutes >= 60)
-        {
-            $degrees += floor($minutes/60.0);
-            $minutes -= 60*floor($minutes/60.0);
-        }
-        if($isflaot){
-            $point = $this->floatGps($degrees,  $minutes, $seconds);
-        } else {
-            $point = array('degrees' => $degrees, 'minutes' => $minutes, 'seconds' => $seconds);
-        }
-        return $point;
-    }
-
-    function gps2Num($coordPart)
-    {
-        $parts = explode('/', $coordPart);
-
-        if(count($parts) <= 0)
-            return 0;
-        if(count($parts) == 1)
-            return $parts[0];
-
-        return floatval($parts[0]) / floatval($parts[1]);
     }
 } 
--- core/common/define.inc.php
+++ core/common/define.inc.php
@@ -16,7 +16,12 @@ defined('CACHE_PATH') or define('CACHE_PATH',__PATH__.'/cache');
 defined('DATA_PATH') or define('DATA_PATH',__PATH__.'/data');
 defined('UPLOAD_PATH') or define('UPLOAD_PATH',DATA_PATH.'/upload');
 $self =explode('/',$_SERVER['PHP_SELF']);
-defined('__ROOT__') or define('__ROOT__',count($self) == 3 ? 'http://'.$_SERVER['SERVER_NAME'] . '/' . $self[1] : 'http://'.$_SERVER['SERVER_NAME']);
+$root = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'];
+$root .= $_SERVER['SERVER_PORT'] <> 80 ? ':'.$_SERVER['SERVER_PORT']:'';
+if(count($self) == 3){
+    $root .= '/' . $self[1];
+}
+defined('__ROOT__') or define('__ROOT__',$root);
 defined('__PUBLIC__') or define('__PUBLIC__',__ROOT__.'/public');
 defined('__JSROOT__') or define('__JSROOT__',__PUBLIC__.'/js');
 defined('__PLROOT__') or define('__PLROOT__',__PUBLIC__.'/plugn');
