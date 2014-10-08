31e4e922ec264a4613a4036e09d0f89d0405d239
liushaochen <liushaochen1987@qq.com>
2014-10-08 11:41:53 +0800

    修改swfupload上传组件的上传流程（文件名规则有修改）；

--- core/class/Upload.class.php
+++ core/class/Upload.class.php
@@ -8,4 +8,138 @@
 
 class Upload {
 
+    public function Upload(){
+        return $this;
+    }
+//判断是否登录
+    function islogin(){
+        if(!$_SESSION['uid']){
+            //没有登录
+            $data = array('status'=>500,'message'=>'no login!','data'=>array());
+        } else {
+            $data = array();
+        }
+        return $data;
+    }
+
+    function masSize(){
+        $POST_MAX_SIZE = ini_get('post_max_size');
+        $unit = strtoupper(substr($POST_MAX_SIZE, -1));
+        $multiplier = ($unit == 'M' ? 1048576 : ($unit == 'K' ? 1024 : ($unit == 'G' ? 1073741824 : 1)));
+
+        if ((int)$_SERVER['CONTENT_LENGTH'] > $multiplier*(int)$POST_MAX_SIZE && $POST_MAX_SIZE) {
+            $data = array('status'=>500,'message'=>'POST exceeded maximum allowed size.','data'=>array());
+        }
+        return $data;
+    }
+
+    function setting($file_model){
+        $this->save_path = __PATH__ . '/data/'.$file_model.'/'.date('Y/m/d/');
+        addDir($this->save_path);
+        $this->upload_name = "Filedata";
+        $this->max_file_size_in_bytes = 2147483647;				// 2GB in bytes
+        $this->extension_whitelist = array("doc", "txt", "jpg", "gif", "png","JPG");	//允许的文件
+        $this->valid_chars_regex = '.A-Z0-9_ !@#$%^&()+={}\[\]\',~`-';				//允许的文件名字符
+        $this->MAX_FILENAME_LENGTH = 260;
+    }
+
+    function settingError(){
+        if (!isset($_FILES[$this->upload_name])) {
+            return array('status'=>500,'message'=>"No upload found in \$_FILES for " . $this->upload_name,'data'=>array());
+        } else if (isset($_FILES[$this->upload_name]["error"]) && $_FILES[$this->upload_name]["error"] != 0) {
+            return array('status'=>500,'message'=>$this->uploadErrors[$_FILES[$this->upload_name]["error"]],'data'=>array());
+        } else if (!isset($_FILES[$this->upload_name]["tmp_name"]) || !@is_uploaded_file($_FILES[$this->upload_name]["tmp_name"])) {
+            return array('status'=>500,'message'=>"Upload failed is_uploaded_file test.",'data'=>array());
+        } else if (!isset($_FILES[$this->upload_name]['name'])) {
+            return array('status'=>500,'message'=>"File has no name.",'data'=>array());
+        }
+        $file_size = @filesize($_FILES[$this->upload_name]["tmp_name"]);
+        if (!$file_size || $file_size > $this->max_file_size_in_bytes) {
+            return array('status'=>500,'message'=>"File exceeds the maximum allowed size",'data'=>array());
+        }
+
+        if ($file_size <= 0) {
+            return array('status'=>500,'message'=>"File size outside allowed lower bound",'data'=>array());
+        }
+    }
+
+    public function uploadFile($model){
+        //实现上传流程
+        $data = $this->islogin();
+        if($data['status'] == '500'){
+            return $this->returnData($data,$model);
+        }
+        $data = $this->masSize();
+        if($data['status'] == '500'){
+            return $this->returnData($data,$model);
+        }
+
+// Settings
+        if(!$_GET['t']){
+            $type = 'image';
+        } else {
+            $type = $_GET['t'];
+        }
+        $this->setting($type);
+// Other variables
+        $this->uploadErrors = array(
+            0=>"文件上传成功",
+            1=>"上传的文件超过了 php.ini 文件中的 upload_max_filesize directive 里的设置",
+            2=>"上传的文件超过了 HTML form 文件中的 MAX_FILE_SIZE directive 里的设置",
+            3=>"上传的文件仅为部分文件",
+            4=>"没有文件上传",
+            6=>"缺少临时文件夹"
+        );
+        $data = $this->settingError();
+        if($data['status'] == '500'){
+            return $this->returnData($data,$model);
+        }
+
+        $file_name = preg_replace('/[^'.$this->valid_chars_regex.']|\.+$/i', "", basename($_FILES[$this->upload_name]['name']));
+        if (strlen($file_name) == 0 || strlen($file_name) > $this->MAX_FILENAME_LENGTH) {
+            return $this->returnData(array('status'=>500,'message'=>"Invalid file name",'data'=>array()),$model);
+        }
+
+        //存储文件名重写
+        $file_name = explode('.',$file_name);
+        $file_name = time().rand(0,99).'.'.$file_name[count($file_name) - 1];
+        //注释掉文件名重复时返回错误信息
+//        if (file_exists($this->save_path . $file_name)) {
+//            return $this->returnData(array('status'=>500,'message'=>"File with this name already exists",'data'=>array()),$model);
+//        }
+
+// Validate file extension
+        $path_info = pathinfo($_FILES[$this->upload_name]['name']);
+        $file_extension = $path_info["extension"];
+        $is_valid_extension = false;
+        foreach ($this->extension_whitelist as $extension) {
+            if (strcasecmp($file_extension, $extension) == 0) {
+                $is_valid_extension = true;
+
+                break;
+            }
+        }
+        if (!$is_valid_extension) {
+            return $this->returnData(array('status'=>500,'message'=>"Invalid file extension",'data'=>array()),$model);
+        }
+
+        if (!@move_uploaded_file($_FILES[$this->upload_name]["tmp_name"], $this->save_path.$file_name)) {
+            $data = array('status'=>500,'message'=>"文件无法保存:".$this->save_path.$file_name,'data'=>array());
+        }else{
+            Log::write('UPLOAD SUCCESS',"文件已保存:".$this->save_path.$file_name,'file');
+            Log::write('UPLOAD SUCCESS',"文件内容:".json_encode($_FILES),'file');
+            $data = array('status'=>200,'message'=>"ok",'data'=>array('save_path'=>$this->save_path,'file_name'=>$file_name));
+        }
+
+        //实现数据回传整理
+        return $this->returnData($data,$model);
+    }
+
+    function returnData($data,$model){
+        switch($model){
+            case 'swfload':$data = array('status'=>$data['status'],'message'=>$data['message'],'data'=>$data['data']);break;
+            default:$data = array('status'=>$data['status'],'message'=>$data['message'],'data'=>$data['data']);
+        }
+        return $data;
+    }
 } 
--- public/plugn/swfupload/upload.php
+++ public/plugn/swfupload/upload.php
@@ -6,8 +6,7 @@ unset($path[count($path)-1]);
 unset($path[count($path)-1]);
 $path = implode('/',$path);
 define("__PATH__",$path);
-include_once('../../../core/start.php');
-
+include_once(__PATH__.'/core/start.php');
 if(empty($charset))  $charset = C('DEFAULT_CHARSET');
 if(empty($contentType)) $contentType = C('TMPL_CONTENT_TYPE');
 // 网页字符编码
@@ -21,106 +20,118 @@ if (isset($_POST["PHPSESSID"])) {
 
 session_start();
 
-$POST_MAX_SIZE = ini_get('post_max_size');
-$unit = strtoupper(substr($POST_MAX_SIZE, -1));
-$multiplier = ($unit == 'M' ? 1048576 : ($unit == 'K' ? 1024 : ($unit == 'G' ? 1073741824 : 1)));
-
-if ((int)$_SERVER['CONTENT_LENGTH'] > $multiplier*(int)$POST_MAX_SIZE && $POST_MAX_SIZE) {
-    header("HTTP/1.1 500 Internal Server Error");
-    echo "POST exceeded maximum allowed size.";
-    exit(0);
+$upload = new Upload();
+$data = $upload->uploadFile('swfload');
+if($data['status'] == 200){
+    echo 'File Received';
+} else {
+    HandleError($data);
 }
+//$POST_MAX_SIZE = ini_get('post_max_size');
+//$unit = strtoupper(substr($POST_MAX_SIZE, -1));
+//$multiplier = ($unit == 'M' ? 1048576 : ($unit == 'K' ? 1024 : ($unit == 'G' ? 1073741824 : 1)));
+//
+//if ((int)$_SERVER['CONTENT_LENGTH'] > $multiplier*(int)$POST_MAX_SIZE && $POST_MAX_SIZE) {
+//    header("HTTP/1.1 500 Internal Server Error");
+//    echo "POST exceeded maximum allowed size.";
+//    exit(0);
+//}
+//
+//// Settings
+//if(!$_GET['t']){
+//    $type = 'image';
+//} else {
+//    $type = $_GET['t'];
+//}
+//$save_path = __PATH__ . '/data/'.$type.'/'.date('Y/m/d/');
+//addDir($save_path);
+//$upload_name = "Filedata";
+//$max_file_size_in_bytes = 2147483647;				// 2GB in bytes
+//$extension_whitelist = array("doc", "txt", "jpg", "gif", "png","JPG");	//允许的文件
+//$valid_chars_regex = '.A-Z0-9_ !@#$%^&()+={}\[\]\',~`-';				//允许的文件名字符
+//
+//// Other variables
+//$MAX_FILENAME_LENGTH = 260;
+//$file_name = "";
+//$file_extension = "";
+//$uploadErrors = array(
+//    0=>"文件上传成功",
+//    1=>"上传的文件超过了 php.ini 文件中的 upload_max_filesize directive 里的设置",
+//    2=>"上传的文件超过了 HTML form 文件中的 MAX_FILE_SIZE directive 里的设置",
+//    3=>"上传的文件仅为部分文件",
+//    4=>"没有文件上传",
+//    6=>"缺少临时文件夹"
+//);
+//
+//if (!isset($_FILES[$upload_name])) {
+//    HandleError("No upload found in \$_FILES for " . $upload_name);
+//    exit(0);
+//} else if (isset($_FILES[$upload_name]["error"]) && $_FILES[$upload_name]["error"] != 0) {
+//    HandleError($uploadErrors[$_FILES[$upload_name]["error"]]);
+//    exit(0);
+//} else if (!isset($_FILES[$upload_name]["tmp_name"]) || !@is_uploaded_file($_FILES[$upload_name]["tmp_name"])) {
+//    HandleError("Upload failed is_uploaded_file test.");
+//    exit(0);
+//} else if (!isset($_FILES[$upload_name]['name'])) {
+//    HandleError("File has no name.");
+//    exit(0);
+//}
+//
+//$file_size = @filesize($_FILES[$upload_name]["tmp_name"]);
+//if (!$file_size || $file_size > $max_file_size_in_bytes) {
+//    HandleError("File exceeds the maximum allowed size");
+//    exit(0);
+//}
+//
+//if ($file_size <= 0) {
+//    HandleError("File size outside allowed lower bound");
+//    exit(0);
+//}
+//
+//
+//$file_name = preg_replace('/[^'.$valid_chars_regex.']|\.+$/i', "", basename($_FILES[$upload_name]['name']));
+//if (strlen($file_name) == 0 || strlen($file_name) > $MAX_FILENAME_LENGTH) {
+//    HandleError("Invalid file name");
+//    exit(0);
+//}
+//
+//
+//if (file_exists($save_path . $file_name)) {
+//    HandleError("File with this name already exists");
+//    exit(0);
+//}
+//
+//// Validate file extension
+//$path_info = pathinfo($_FILES[$upload_name]['name']);
+//$file_extension = $path_info["extension"];
+//$is_valid_extension = false;
+//foreach ($extension_whitelist as $extension) {
+//    if (strcasecmp($file_extension, $extension) == 0) {
+//        $is_valid_extension = true;
+//
+//        break;
+//    }
+//}
+//if (!$is_valid_extension) {
+//    HandleError("Invalid file extension");
+//    exit(0);
+//}
+//
+//
+//if (!@move_uploaded_file($_FILES[$upload_name]["tmp_name"], $save_path.$file_name)) {
+//    HandleError("文件无法保存:".$save_path.$file_name);
+//    exit(0);
+//}else{
+//
+//}
 
-// Settings
-$save_path = __PATH__ . "/file/";Log::write('file',$save_path,'file');
-addDir($save_path);
-$upload_name = "Filedata";
-$max_file_size_in_bytes = 2147483647;				// 2GB in bytes
-$extension_whitelist = array("doc", "txt", "jpg", "gif", "png","JPG");	//允许的文件
-$valid_chars_regex = '.A-Z0-9_ !@#$%^&()+={}\[\]\',~`-';				//允许的文件名字符
-
-// Other variables	
-$MAX_FILENAME_LENGTH = 260;
-$file_name = "";
-$file_extension = "";
-$uploadErrors = array(
-    0=>"文件上传成功",
-    1=>"上传的文件超过了 php.ini 文件中的 upload_max_filesize directive 里的设置",
-    2=>"上传的文件超过了 HTML form 文件中的 MAX_FILE_SIZE directive 里的设置",
-    3=>"上传的文件仅为部分文件",
-    4=>"没有文件上传",
-    6=>"缺少临时文件夹"
-);
-
-if (!isset($_FILES[$upload_name])) {
-    HandleError("No upload found in \$_FILES for " . $upload_name);
-    exit(0);
-} else if (isset($_FILES[$upload_name]["error"]) && $_FILES[$upload_name]["error"] != 0) {
-    HandleError($uploadErrors[$_FILES[$upload_name]["error"]]);
-    exit(0);
-} else if (!isset($_FILES[$upload_name]["tmp_name"]) || !@is_uploaded_file($_FILES[$upload_name]["tmp_name"])) {
-    HandleError("Upload failed is_uploaded_file test.");
-    exit(0);
-} else if (!isset($_FILES[$upload_name]['name'])) {
-    HandleError("File has no name.");
-    exit(0);
-}
-
-$file_size = @filesize($_FILES[$upload_name]["tmp_name"]);
-if (!$file_size || $file_size > $max_file_size_in_bytes) {
-    HandleError("File exceeds the maximum allowed size");
-    exit(0);
-}
-
-if ($file_size <= 0) {
-    HandleError("File size outside allowed lower bound");
-    exit(0);
-}
-
-
-$file_name = preg_replace('/[^'.$valid_chars_regex.']|\.+$/i', "", basename($_FILES[$upload_name]['name']));
-if (strlen($file_name) == 0 || strlen($file_name) > $MAX_FILENAME_LENGTH) {
-    HandleError("Invalid file name");
-    exit(0);
-}
-
-
-if (file_exists($save_path . $file_name)) {
-    HandleError("File with this name already exists");
-    exit(0);
-}
-
-// Validate file extension
-$path_info = pathinfo($_FILES[$upload_name]['name']);
-$file_extension = $path_info["extension"];
-$is_valid_extension = false;
-foreach ($extension_whitelist as $extension) {
-    if (strcasecmp($file_extension, $extension) == 0) {
-        $is_valid_extension = true;
-
-        break;
-    }
-}
-if (!$is_valid_extension) {
-    HandleError("Invalid file extension");
-    exit(0);
-}
-
-
-if (!@move_uploaded_file($_FILES[$upload_name]["tmp_name"], $save_path.$file_name)) {
-    HandleError("文件无法保存:".$save_path.$file_name);
-    exit(0);
-}else{
-
-}
-
-echo "File Received";
-exit(0);
+//echo "File Received";
+//exit(0);
 
 
 function HandleError($message) {
     header("HTTP/1.1 500 Internal Server Error");
-    $str = date('Y-m-d H:i:s').$message."\r\n";
+    $str = date('Y-m-d H:i:s').$message['message']."\r\n";
     $str .= 'max_execution_time = ' . ini_get('max_execution_time')."\r\n";
     $str .=  'memory_limit = ' . ini_get('memory_limit')."\r\n";
     $str .=  'upload_max_filesize = ' . ini_get('upload_max_filesize')."\r\n";
@@ -128,7 +139,7 @@ function HandleError($message) {
     $str .=  'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "/n";
     $str .=  'post_max_size in bytes = ' . return_bytes(ini_get('post_max_size'))."\r\n";
     Log::write('UPLOAD ERROR',$str,'file');
-    echo $message;
+    echo $message['message'];
 }
 function return_bytes($val) {
     $val = trim($val);
