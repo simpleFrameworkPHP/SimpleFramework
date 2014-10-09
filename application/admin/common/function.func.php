e7ca215fa39783013b82aa8f929dadcf16fe438b
liushaochen <liushaochen1987@qq.com>
2014-10-09 09:16:39 +0800

    后台主页样式以及菜单导航栏修改；

--- application/admin/common/function.func.php
+++ application/admin/common/function.func.php
@@ -4,8 +4,8 @@ function getMenu($model,$target = ''){
     $str = '';
     switch($model){
         case 'admin_main':
-            $html = '<li><a href=menu_url '.($target <> ''?'target="'.$target.'"':'').' onclick="clickMenu(menu_0,this)">menu_title</a></li>';
-            $data = array(array('0'=>'index','name'=>'百度','url'=>'http://www.baidu.com'),array('0'=>'admin','name'=>'百度admin','url'=>'http://www.baidu.com'));
+            $html = '<li><a id=menu_0 href=menu_url '.($target <> ''?'target="'.$target.'"':'').' onclick="clickMenu(menu_0)">menu_title</a></li>';
+            $data = array(array('0'=>'index','name'=>'系统','url'=>'http://www.baidu.com'),array('0'=>'admin','name'=>'用户','url'=>'http://www.baidu.com'),array('0'=>'content','name'=>'内容','url'=>'http://www.baidu.com'));
             foreach($data as $i){
                 $selected = $_REQUEST['c'] == $i['0'] ? ' class="selected" ' : '';
                 $ihtml = str_replace('menu_url','"'.$i['url'].'" '.$selected,$html);
@@ -17,11 +17,13 @@ function getMenu($model,$target = ''){
         case 'admin':
             $data = array(
                 'index'=>array(
-                    array('0'=>'index','name'=>'百度','url'=>'http://www.baidu.com'),
-                    array('0'=>'admin','name'=>'百度admin','url'=>'http://www.baidu.com')
+                    array('0'=>'index','name'=>'系统介绍','url'=>'http://www.baidu.com')
                 ),
                 'admin'=>array(
                     array('0'=>'user','name'=>'用户管理','url'=>'http://www.baidu.com')
+                ),
+                'content'=>array(
+                    array('0'=>'content','name'=>'内容管理','url'=>'http://www.baidu.com')
                 )
             );
             $str = json_encode($data);
