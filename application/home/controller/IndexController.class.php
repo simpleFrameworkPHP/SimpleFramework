19c6d6553b6e979c10396ba954365931f2fe0e61
liushaochen <liushaochen1987@qq.com>
2014-10-09 17:33:47 +0800

    前台登陆流程；

--- application/home/controller/IndexController.class.php
+++ application/home/controller/IndexController.class.php
@@ -15,7 +15,18 @@ class IndexController extends Controller {
     }
 
     public function login(){
-
+        if($_POST){
+            $where['username'] = $_POST['username'];
+            $where['password'] = $_POST['password'];
+            $user = M()->table('sf_user')->where($where)->find();
+            if($user['id']){
+                $_SESSION['uid'] = $user['id'];
+                redirect(H('home/index/index'));
+            } else {
+                echo '用户名或密码错误';
+            }
+        }
+        $this->display();
     }
 
     public function register(){
