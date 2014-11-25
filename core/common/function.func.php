978dd6085d504db09d7b1104fd31f5b6a6138fa4
liushaochen <liushaochen1987@qq.com>
2014-11-25 09:33:28 +0800

    修改创建文件夹方法；

--- core/common/function.func.php
+++ core/common/function.func.php
@@ -133,7 +133,7 @@ function addDir($file){
     foreach($dir_array as $value){
         $idir .= $value . '/';
         if(!is_dir($idir)){
-            @mkdir($idir,0777);
+            mkdir($idir,777);
         }
     }
     return is_dir($dir);
