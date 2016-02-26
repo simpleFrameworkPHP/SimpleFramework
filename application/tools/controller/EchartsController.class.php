d992cb670721be316a0ccaa274d464e2a65bcce2
liushaochen <liushaochen1987@qq.com>
2016-02-26 16:51:07 +0800

    1. 添加图表示例；

---
+++ application/tools/controller/EchartsController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liushaochen
+ * Date: 16/2/26
+ * Time: 上午11:21
+ */
+
+class EchartsController extends Controller {
+
+    public function index(){
+        $this->display();
+    }
+} 
