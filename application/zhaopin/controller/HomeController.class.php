c21194a0703cfd3731442869c7aba8edd8cfac1b
liushaochen <liushaochen1987@qq.com>
2016-03-11 11:15:56 +0800

    1. 整体样式修正； 2. 添加年限&薪资范围分布===》职位数&平均薪资；

--- application/zhaopin/controller/HomeController.class.php
+++ application/zhaopin/controller/HomeController.class.php
@@ -36,7 +36,7 @@ class HomeController extends Controller {
         }
         $list[] = array("data"=>$positions,"name"=>"职位数","type"=>"bar");
         $list[] = array("data"=>$companys,"name"=>"公司数","type"=>"bar");
-        $list[] = array("data"=>$cp,"name"=>"职位/公司数","type"=>"line","yAxisIndex"=>1);
+        $list[] = array("data"=>$cp,"name"=>"职位数/公司数","type"=>"line","yAxisIndex"=>1);
         $this->assign('json',JSON($list));
         $this->assign('xAxis',JSON($city));
         $this->assign('item',JSON(array("职位数","公司数","职位数/公司数")));
@@ -50,24 +50,59 @@ class HomeController extends Controller {
      */
     public function indexWorkYearNP(){
         //$where['add_time'] = date('Y-m-d');
-        $model = M('',3);
-        $data = $model->table("model_city")
-            //->where($where)
-            ->limit(5)
-            ->order(array("countPosition"=>"DESC"))
-            ->select();
-        foreach($data as $row){
-            $city[] = $row['city'];
-            $positions[] = $row['countPosition'];
-            $companys[] = $row['countCompany'];
-            $cp[] = $row['countPosition']/$row['countCompany'];
+        $workYear = array(0,1,2,3,4,5,6,7,8,9,10);
+        $bei = 5000;
+        foreach($workYear as $key=>$value){
+            $salary[$key] = $value * $bei;
         }
-        $list[] = array("data"=>$positions,"name"=>"职位数","type"=>"bar");
-        $list[] = array("data"=>$companys,"name"=>"公司数","type"=>"bar");
-        $list[] = array("data"=>$cp,"name"=>"职位/公司数","type"=>"line","yAxisIndex"=>1);
-        $this->assign('json',JSON($list));
-        $this->assign('xAxis',JSON($city));
-        $this->assign('item',JSON(array("职位数","公司数","职位/公司数")));
+        $salary = array_values($salary);
+        $model = M('',3);
+        $result = $model->table("model_position")
+            //->where($where)
+            ->fields('positionId,minWorkYear,maxWorkYear,minSalary,maxSalary')
+            ->select();
+        $item = $salary;
+        $list = array();
+        foreach($result as $row){
+            foreach($salary as $skey=>$svalue){
+                if($row['minSalary'] == 0 || $row['minSalary'] <= $svalue){
+                    if($row['maxSalary'] == 0 || $row['maxSalary'] >= $svalue){
+                        foreach($workYear as $nkey=>$nvalue){
+                            if($row['minWorkYear'] == 0 || $row['minWorkYear'] <= $nvalue){
+                                if($row['maxWorkYear'] == 0 || $row['maxWorkYear'] >= $nvalue){
+                                    $list[$svalue][$nvalue]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        //工作年限&cp
+        $iyear = array();
+        //工作年限&薪资
+        $isalary = array();
+        foreach($list as $salary=>$value){
+            foreach($value as $year=>$cp){
+                $iyear[$year] += $cp;
+                $isalary[$year] += $cp*$salary;
+            }
+        }
+        $avgSalary = array();
+        foreach($iyear as $year => $value){
+            $avgSalary[$year] = number_format($isalary[$year] / $iyear[$year],2,'.','');
+        }
+        $data[] = array("data"=>array_values($avgSalary),"name"=>'工作年限&平均工资',"type"=>"line","yAxisIndex"=>1);
+        foreach($workYear as $value){
+            $xAxis[] = $value.'年';
+        }
+
+        foreach($list as $key => $value){
+            $data[] = array("data"=>array_values($value),"name"=>$key,"type"=>"bar");
+        }
+        $this->assign('json',JSON($data));
+        $this->assign('xAxis',JSON($xAxis));
+        $this->assign('item',JSON($item));
         $this->assign('id','workyear_np');
         $this->assign('title','【数据挖掘】按照工作年限统计薪资分布');
         return $this->fetch('index_dy_echarts');
