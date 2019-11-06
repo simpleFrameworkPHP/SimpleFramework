91da0dd21e1d6791646642c28dee96a0a0bc05a8
Bun <liushaochen1987@qq.com>
2019-11-06 15:19:23 +0800

    Update config.inc.php

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -13,23 +13,6 @@ return array(
 
 
     'SF_DB_CONNECT'=>array(
-        1 => array(
-//            'DB_HOST'=>'www.jiemo.net',//'218.60.94.27',
-            'DB_HOST'=>'218.60.94.27',
-            'DB_NAME'=>'jiemo',
-            'DB_PORT'=>3306,
-            'DB_USER'=>'jiemo',
-            'DB_PASS'=>'jiemo@qwer',
-            'DB_MODE'=>'mysqli',
-        ),
-       2 => array(
-            'DB_HOST'=>'218.60.94.27',
-            'DB_NAME'=>'test',
-            'DB_PORT'=>3306,
-            'DB_USER'=>'jiemo',
-            'DB_PASS'=>'jiemo@qwer',
-            'DB_MODE'=>'mysqli',
-       ),
         4 => array(
             'DB_HOST'=>'127.0.0.1',
             'DB_NAME'=>'note',
